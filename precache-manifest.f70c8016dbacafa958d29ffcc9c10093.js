7c7d38fa8552bf4f32384c19c0daa2a8e0bd69ad
edwardcashmere <eomondi06@gmail.com>
2020-11-18 11:11:26 +0300

    Updates

---
+++ precache-manifest.f70c8016dbacafa958d29ffcc9c10093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f782fb48f73fd75307a009cdb538606",
+    "url": "/Covid-Tracker-React/index.html"
+  },
+  {
+    "revision": "489bfd5c362f4ce11438",
+    "url": "/Covid-Tracker-React/static/css/main.d65d36b4.chunk.css"
+  },
+  {
+    "revision": "1d92dda60385a9ac0548",
+    "url": "/Covid-Tracker-React/static/js/2.b1011e0c.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-Tracker-React/static/js/2.b1011e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489bfd5c362f4ce11438",
+    "url": "/Covid-Tracker-React/static/js/main.d148e14a.chunk.js"
+  },
+  {
+    "revision": "de648fc4f46a663a3df1",
+    "url": "/Covid-Tracker-React/static/js/runtime-main.1be1b7fc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker-React/static/media/covid.d7265326.png"
+  }
+]);
